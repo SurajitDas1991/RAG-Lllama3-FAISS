d5c5e6a597d8efd9f5baa8809503cc01ffd58ec7
Surajit Das <dsurajitd@gmail.com>
2024-09-22 15:36:58 +0530

    Added functionality to have a conversation with the chat model

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# A simple RAG implementation to take in a single pdf doc and have a one-off conversation with the model.
+# A conversational RAG implementation to take in a multiple PDF docs and have a conversation with the model based on the contents available in the pdfs
 
 ### Tools Used
 
 - [Ollama Llama3.1](https://ollama.com/library/llama3.1)
 - [FAISS](https://faiss.ai/index.html)
 - [Langchain](https://www.langchain.com/)
+
+
+### RAG Pipeline
+
+- The tool uses HuggingFace embeddings,FAISS vector store and uses a local Ollama Llama3.1 model to generate responses to queries. The dataset consists of multiple pdfs in the 'pdfs' folder.
+
+- For a conversational application, storing the chat context is important and retrieving the same during conversation is what makes a chat model successful. For this langchain provides a retriever - **create_history_aware_retriever** -> this takes in the chat history and formulates a query to the vector store retriever so that the retrieval incorporates the context of the conversation. For the very first time the app gets launched, there would be no chat history and the default query will be passed on to the retriever. This returns a list of documents.
+
+- Once we have the list of documents, we pass it onto a Question Answering Chain via **create_stuff_documents_chain** which takes in the context passed in along with a prompt along with te conversation history
+
+- **create_retrieval_chain** is then used to apply **create_history_aware_retriever** and **create_stuff_documents_chain** in sequence. It has input keys input and chat_history, and includes input, chat_history, context, and answer in its output.
+
+
+### Code setup
+
+- Create a virtual environment using a python version >=3.12 and activate it.
+- Install the requirements using requirements.txt
+- cd to the directory containing main.py
+- start the app using the following command:
+  ```python
+    python .\main.py
+  ```
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # GenAI libraries
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_community.llms import Ollama
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.base import RunnableSequence
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.schema.output_parser import StrOutputParser
 
 # Non GenAI libraries
@@ -17,18 +25,34 @@ from typing import List
 import os
 
 
-def get_pdf_path(name: str) -> str:
-    return str(Path.joinpath(Path.cwd(), name))
+def get_pdf_path(name: str) -> List[str]:
+    print("Getting PDF files..")
+    path = Path.joinpath(Path.cwd(), name)
+    list_pdf_files = []
+    for _, _, file in os.walk(path):
+        for f in file:
+            if ".pdf" in f:
+                list_pdf_files.append(Path.joinpath(Path.cwd(), name, f))
+                # print(f)
+    return list_pdf_files
 
+def flatten_comprehension(matrix):
+    return [item for row in matrix for item in row]
 
-def load_pdf(filepath: str) -> List[Document]:
-    loader = PyPDFLoader(filepath)
-    documents = loader.load()
-    # print(documents[0])
-    return documents
+def load_pdf(pdf_docs: List[str]) -> List[Document]:
+    print("Loading PDFs..")
+    all_documents = []
+    for pdf in pdf_docs:
+        loader = PyPDFLoader(pdf, extract_images=False)
+        all_documents.append(loader.load())
+    # print(type(all_documents))
+    docs = flatten_comprehension(all_documents)
+    # print(docs[0])
+    return docs
 
 
 def preprocess_file(documents: List[Document]) -> List[Document]:
+    print("Preprocessing documents")
     text_splitter = CharacterTextSplitter(
         chunk_size=500, chunk_overlap=30, separator="\n"
     )
@@ -37,22 +61,26 @@ def preprocess_file(documents: List[Document]) -> List[Document]:
 
 
 def load_embeddings() -> HuggingFaceEmbeddings:
+    print("Loading embeddings")
     embedding_model_name = "BAAI/bge-m3"
     embeddings = HuggingFaceEmbeddings(
         model_name=embedding_model_name,
     )
-    print(type(embeddings))
     return embeddings
 
 
 def setup_vector_store(docs: List[Document], embeddings: HuggingFaceEmbeddings) -> str:
-    vector_store = FAISS.from_documents(docs, embeddings)
-    if not os.path.exists(Path.joinpath(Path.cwd(), "faiss_index")):
-        vector_store.save_local("faiss_index")
-    return str(Path.joinpath(Path.cwd(), "faiss_index"))
+    if not os.path.exists(Path.joinpath(Path.cwd(), "faiss_pdf_store")):
+        print("Setting up vector store")
+        vector_store = FAISS.from_documents(docs, embeddings)
+        vector_store.save_local("faiss_pdf_store")
+    else:
+        print("Vector store already exists..")
+    return str(Path.joinpath(Path.cwd(), "faiss_pdf_store"))
 
 
 def load_vector_store(filepath: str) -> FAISS:
+    print("Loading vector store")
     if os.path.exists(filepath):
         persisted_vector_store = FAISS.load_local(
             filepath, embeddings, allow_dangerous_deserialization=True
@@ -61,22 +89,15 @@ def load_vector_store(filepath: str) -> FAISS:
 
 
 def load_ollama() -> Ollama:
+    print("Loading Ollama model")
     llm = Ollama(model="llama3.1")
-    # response = llm.invoke("Tell me a joke")
-    # print(response)
     return llm
 
 
-def create_chain(llm_model) -> RunnableSequence:
-    prompt_template = ChatPromptTemplate.from_messages(
-        "Why do software designers struggle with system design interviews?"
-    )
-    chain = prompt_template | llm_model | StrOutputParser()
-    print(type(chain))
-    return chain
 
 
 def retrieve_from_db(db: FAISS) -> VectorStoreRetriever:
+    print("Getting the retriever")
     # Retrieve relevant documents based on the query
     retriever = db.as_retriever(
         search_type="similarity",
@@ -85,43 +106,101 @@ def retrieve_from_db(db: FAISS) -> VectorStoreRetriever:
     return retriever
 
 
-def combine_query_relevant_chunks(query: str, relevant_docs: List[Document]):
-    combined_input = (
-        "Here are some documents that might help answer the question: "
-        + query
-        + "\n\nRelevant Documents:\n"
-        + "\n\n".join([doc.page_content for doc in relevant_docs])
-        + "\n\nPlease provide an answer based only on the provided documents. If the answer is not found in the documents, respond with 'I'm not sure'."
+def converse_with_model(rag_chain):
+    print("Start chatting with the AI! Type 'exit' to end the conversation.")
+    # chat_history = []  # Collect chat history here (a sequence of messages)
+    while True:
+        query = input("You: ")
+        if query.lower() == "exit":
+            break
+        # Process the user's query through the retrieval chain
+        # result = rag_chain.invoke({"input": query, "chat_history": chat_history})
+        result = rag_chain.invoke(
+            {"input": query}, config={"configurable": {"session_id": "1"}}
+        )
+        # Display the AI's response
+        print(f"AI: {result['answer']}")
+
+
+def chat_history_aware_retriever(llm: Ollama, retriever):
+    print("Creating history aware retriever")
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, just "
+        "reformulate it if needed and otherwise return it as is."
     )
-    return combined_input
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    return history_aware_retriever
 
 
-def invoke_model(llm: Ollama, combined_input: str):
-    messages = [
-        SystemMessage(content="You are a helpful assistant."),
-        HumanMessage(content=combined_input),
-    ]
-    result = llm.invoke(messages)
-    return result
+def create_documents_chain(llm: Ollama):
+    """https://python.langchain.com/v0.2/docs/how_to/qa_chat_history_how_to/"""
+    print("Creating document chain")
+    qa_system_prompt = (
+        "You are an assistant for question-answering tasks. Use "
+        "the following pieces of retrieved context to answer the "
+        "question. If you don't know the answer, just say that you "
+        "don't know. Use ten sentences maximum and keep the answer "
+        "concise."
+        "\n\n"
+        "{context}"
+    )
+
+    # Create a prompt template for answering questions
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    # Create a chain to combine documents for question answering
+    # `create_stuff_documents_chain` feeds all retrieved context into the LLM
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    return question_answer_chain
+
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    print("Getting/Creating session history")
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 
 if __name__ == "__main__":
-    file_path = get_pdf_path("system-design.pdf")
-    documents = load_pdf(file_path)
-    chunked_docs = preprocess_file(documents)
-    embeddings = load_embeddings()
-    vector_store_path = setup_vector_store(chunked_docs, embeddings)
-    vector_store = load_vector_store(vector_store_path)
-    retriever = retrieve_from_db(vector_store)
-    query = "Why do software designers struggle with system design interviews?"
-    relevant_docs = retriever.invoke(query)
-    print(type(relevant_docs))
-    # Display the relevant results with metadata
-    print("\n--- Relevant Documents ---")
-    for i, doc in enumerate(relevant_docs, 1):
-        print(f"Document {i}:\n{doc.page_content}\n")
-    final_query = combine_query_relevant_chunks(query, relevant_docs)
-    llm = load_ollama()
-    result = invoke_model(llm, final_query)
-    print("Content only:")
-    print(result)
+    file_path = get_pdf_path("pdfs")
+    if (len(file_path)) != 0:
+        documents = load_pdf(file_path)
+        chunked_docs = preprocess_file(documents)
+        embeddings = load_embeddings()
+        vector_store_path = setup_vector_store(chunked_docs, embeddings)
+        vector_store = load_vector_store(vector_store_path)
+        retriever = retrieve_from_db(vector_store)
+        llm = load_ollama()
+        final_rag_chain = create_retrieval_chain(
+            chat_history_aware_retriever(llm, retriever), create_documents_chain(llm)
+        )
+        conversational_rag_chain = RunnableWithMessageHistory(
+            final_rag_chain,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+            output_messages_key="answer",
+        )
+        converse_with_model(conversational_rag_chain)
+    else:
+        print("No pdf files found!!")
